b6e9cc657428a8a848c3c56e32138e02e723472e
MarwanYoOousry <marwany@email.com>
2022-10-15 15:52:55 +0200

    Here I get All Users

--- src/main/java/com/example/spaintask/controllers/UserController.java
+++ src/main/java/com/example/spaintask/controllers/UserController.java
@@ -5,9 +5,12 @@ import com.example.spaintask.models.userModel.User;
 import com.example.spaintask.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -22,11 +25,12 @@ public class UserController {
         return "Wellcome to Manging User - Service ";
 
     }
-
-    @PostMapping("/adduser")
-    public User addUser(@Valid @RequestBody User newUser){
-        return userService.createUser(newUser);
+    @RequestMapping(value = "/user/allusers",method =RequestMethod.GET )
+    public ResponseEntity<Object> getUsers(){
+        List<User> userList = userService.getUsers();
+        return new ResponseEntity<>(userList, HttpStatus.ACCEPTED);
     }
+
 
 
 
